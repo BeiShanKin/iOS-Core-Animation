b42756a00564c4366503153f073b336ca0039a24
songyu <syiosengineer1992@gmail.com>
2016-04-27 10:47:23 +0800

    7.1，7.2两个实验添加

--- 07-ImplicitAnimation/07-ImplicitAnimation/ColorChangeViewController.swift
+++ 07-ImplicitAnimation/07-ImplicitAnimation/ColorChangeViewController.swift
@@ -12,10 +12,23 @@ class ColorChangeViewController: UIViewController {
 
     var colorLayer: CALayer?
     @IBAction func colorChangeClick(sender: UIButton) {
+        //CATransaction.begin() 和 CATransaction.commit()可以不用（要用就要成对出现），仅仅使用 CATransaction.setAnimationDuration(5.0)来设置时间
 //        CATransaction.begin()
         CATransaction.setAnimationDuration(5.0)
+        
+        CATransaction.setCompletionBlock { () -> Void in
+            // 如果这里不设置时间，会使用默认的0.25s。这是因为完成块是在颜色渐变的事务提交并出栈之后才被执行，于是，用默认的事务做变换，默认的时间也就变成了0.25秒
+//            CATransaction.setAnimationDuration(1.0)
+            self.colorLayer?.opacity = 0.1
+        }
+        
         self.colorLayer?.backgroundColor = UIColor.blueColor().CGColor
-        self.colorLayer?.opacity = 0.1
+        //setCompletionBlock一定要在改变颜色前面调用，不然就会在颜色改变之前就调用了。
+//        CATransaction.setCompletionBlock { () -> Void in
+//            CATransaction.setAnimationDuration(1.0)
+//            self.colorLayer?.opacity = 0.1
+//        }
+        
 //        CATransaction.commit()
     }
     
