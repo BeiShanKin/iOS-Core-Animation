5afc0d2a795888b7dfe15010384c40dc9e07f8b0
songyu <syiosengineer1992@gmail.com>
2016-05-05 17:14:44 +0800

    11.1 CADisplayLink实验添加

--- 11-FrameAnimation/11-FrameAnimation/CADisplayViewController.swift
+++ 11-FrameAnimation/11-FrameAnimation/CADisplayViewController.swift
@@ -1,29 +1,22 @@
 //
-//  TimerViewController.swift
+//  CADisplayViewController.swift
 //  11-FrameAnimation
 //
 //  Created by lz on 16/5/5.
 //  Copyright © 2016年 SY. All rights reserved.
-/*
-当你设置一个NSTimer，他会被插入到当前任务列表中，然后直到指定时间过去之后才会被执行。但是何时启动定时器并没有一个时间上限，而且它只会在列表中上一个任务完成之后开始执行。这通常会导致有几毫秒的延迟，但是如果上一个任务过了很久才完成就会导致延迟很长一段时间。
-屏幕重绘的频率是一秒钟六十次，但是和定时器行为一样，如果列表中上一个执行了很长时间，它也会延迟。这些延迟都是一个随机值，于是就不能保证定时器精准地一秒钟执行六十次。有时候发生在屏幕重绘之后，这就会使得更新屏幕会有个延迟，看起来就是动画卡壳了。有时候定时器会在屏幕更新的时候执行两次，于是动画看起来就跳动了。
-我们可以通过一些途径来优化：
-我们可以用CADisplayLink让更新频率严格控制在每次屏幕刷新之后。
-基于真实帧的持续时间而不是假设的更新频率来做动画。
-调整动画计时器的run loop模式，这样就不会被别的事件干扰。
-*/
+//
 
 import UIKit
 
-class TimerViewController: UIViewController {
-    
-    
+class CADisplayViewController: UIViewController {
+
     @IBOutlet weak var containerView: UIView!
     
     var ballImageView: UIImageView?
-    var timer: NSTimer?
-    var duration: NSTimeInterval?
-    var timeOffset: NSTimeInterval?
+    var timer: CADisplayLink?
+    var duration: CFTimeInterval?
+    var timeOffset: CFTimeInterval?
+    var lastStep: CFTimeInterval?
     var from: NSValue?
     var to: NSValue?
     
@@ -40,15 +33,41 @@ class TimerViewController: UIViewController {
         self.from = NSValue.init(CGPoint: CGPoint(x: 150, y: 32))
         self.to = NSValue.init(CGPoint: CGPoint(x: 150, y: 268))
         self.timer?.invalidate()
-        self.timer = NSTimer.scheduledTimerWithTimeInterval(1 / 60.0, target: self, selector: "step:", userInfo: nil, repeats: true)
+        self.lastStep = CACurrentMediaTime()
+        self.timer = CADisplayLink.init(target: self, selector: "step:")
+        /*
+        NSDefaultRunLoopMode - 标准优先级
+        NSRunLoopCommonModes - 高优先级
+        UITrackingRunLoopMode - 用于UIScrollView和别的控件的动画
+        
+        在我们的例子中，我们是用了NSDefaultRunLoopMode，但是不能保证动画平滑的运行，所以就可以用NSRunLoopCommonModes来替代。但是要小心，因为如果动画在一个高帧率情况下运行，你会发现一些别的类似于定时器的任务或者类似于滑动的其他iOS动画会暂停，直到动画结束。+
+        
+        同样可以同时对CADisplayLink指定多个run loop模式，于是我们可以同时加入NSDefaultRunLoopMode和UITrackingRunLoopMode来保证它不会被滑动打断，也不会被其他UIKit控件动画影响性能，像这样：
+        self.timer = [CADisplayLink displayLinkWithTarget:self selector:@selector(step:)];
+        [self.timer addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+        [self.timer addToRunLoop:[NSRunLoop mainRunLoop] forMode:UITrackingRunLoopMode];
+        
+        和CADisplayLink类似，NSTimer同样也可以使用不同的run loop模式配置，通过别的函数，而不是+scheduledTimerWithTimeInterval:构造器
+        self.timer = [NSTimer timerWithTimeInterval:1/60.0
+        target:self
+        selector:@selector(step:)
+        userInfo:nil
+        repeats:YES];
+        [[NSRunLoop mainRunLoop] addTimer:self.timer
+        forMode:NSRunLoopCommonModes];
+        */
+        self.timer?.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
     }
     
-    func step(step: NSTimer) {
-        self.timeOffset = min(self.timeOffset! + 1/60.0, self.duration!)
+    func step(step: CADisplayLink) {
+        let thisStep = CACurrentMediaTime()
+        let stepDuration = thisStep - self.lastStep!
+        self.lastStep = thisStep
+        self.timeOffset = min(self.timeOffset! + stepDuration, self.duration!)
         var time = Float(self.timeOffset! / self.duration!)
         time = bounceEaseOut(time)
         let position = interpolateFromValue((self.from?.CGPointValue())!, toValue: (self.to?.CGPointValue())!, time: CGFloat(time))
-        print(position)
+        print(self.lastStep)
         self.ballImageView?.center = position
         if (self.timeOffset >= self.duration) {
             self.timer?.invalidate()
@@ -86,7 +105,7 @@ class TimerViewController: UIViewController {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         animation()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
